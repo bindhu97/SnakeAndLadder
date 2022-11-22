9b4710cada4682ccb0f38df41d846854917476ed
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-22 13:09:25 +0530

    Random Value

---
+++ SnakeAndLadder/src/main/java/com/bridgelabz/SnakeAndLadder/RandomValue.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.SnakeAndLadder;
+
+import java.util.Random;
+public class RandomValue {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6)+1;
+        System.out.println("Dice Value is " +diceValue);
+    }
+}
