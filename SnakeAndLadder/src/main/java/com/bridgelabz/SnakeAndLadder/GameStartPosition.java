7267a3b25e14631b2af45e1371559e7d58a444db
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-22 08:41:07 +0530

    Start Position

---
+++ SnakeAndLadder/src/main/java/com/bridgelabz/SnakeAndLadder/GameStartPosition.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class GameStartPosition {
+    public static void main(String[] args) {
+
+	    int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at the start position "+ pos);
+        
+    }
+    
+}
